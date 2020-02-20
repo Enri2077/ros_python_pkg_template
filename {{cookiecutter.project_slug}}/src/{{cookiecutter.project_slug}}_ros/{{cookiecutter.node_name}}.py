678bcbd67524d81c844fa36ea1b5a24e5b637a00
Enri2077 <enry2077@gmail.com>
2020-02-20 14:56:03 +0100

    adjusted code in python class

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}_ros/{{cookiecutter.node_name}}.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}_ros/{{cookiecutter.node_name}}.py
@@ -3,27 +3,17 @@
 from __future__ import print_function
 
 import rospy
-
+from std_msgs.msg import String
 
 class {{cookiecutter.class_name}}:
     def __init__(self):
         pass
 
-    def image_filename(self, index):
-            return self.image_file_format_string % index
-
-    def info_filename(self, index):
-            return self.info_file_format_string % index
-
     def create_subscriber(self):
-        self.camera_sub_ = rospy.Subscriber(self.camera_topic, Image, self.image_callback)
+        self.sub_ = rospy.Subscriber(self.topic_name, String, self._callback)
 
-    def image_callback(self, msg):
-        if self.use_timestamps_file:
-            if msg.header.stamp in self.timestamps:
-                self.process_image(msg, self.timestamps[msg.header.stamp])
-        else:
-            self.image = msg
+    def _callback(self, msg):
+        pass
 
     def loop(self):
         rospy.spin()
